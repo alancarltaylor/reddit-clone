f8ad97e33e68032f93c1336031ea9a88a4620a86
Alan Taylor <alancarltaylor@gmail.com>
2016-04-30 14:54:02 -0600

    comments working

--- bundle.js
+++ bundle.js
@@ -14,17 +14,37 @@ app.controller("Posts", ["$scope", function($scope){
   }
 
   $scope.addPost = function(newPost){
-    
+
     newPost.dateAdded = moment().calendar();
-    newPost.comments = ['comment'];
     newPost.id = $scope.posts.length;
     newPost.score = 0;
+    newPost.comments = [];
+    newPost.date = new Date();
+    newPost.addComment = false;
+    newPost.showComments = false;
+    newPost.newCommentAuthor = "";
+    newPost.newCommentComment = "";
     $scope.posts.push(newPost);
     console.log(newPost);
     console.log($scope.posts);
     $scope.newPost = {};
     $scope.view.createPost = false;
 
+  }
+  $scope.addComment = function(id){
+    var index = id;
+    var commentObj = {
+      author: $scope.posts[index].newCommentAuthor,
+      comment: $scope.posts[index].newCommentComment
+    };
+    for(var i = 0; i < $scope.posts.length; i++){
+      if($scope.posts[i].id === id){
+        $scope.posts[i].comments.push(commentObj);
+      }
+    }
+    $scope.newCommentAuthor = "";
+    $scope.newCommentComment = "";
+    $scope.posts[index].addComment = false;
   }
 
   $scope.posts = [
@@ -36,7 +56,12 @@ app.controller("Posts", ["$scope", function($scope){
       image: "http://l7.alamy.com/zooms/4e6ed20aab9e4c18a19a5ef2081bf70f/maroon-bells-area-outside-aspen-in-the-maroon-bells-snowmass-wilderness-eb3man.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       score: 0,
-      comments: []
+      comments: [],
+      date: new Date(),
+      addComment: false,
+      showComments: false,
+      newCommentAuthor: "",
+      newCommentComment: ""
     },
     {
       id: 1,
@@ -46,7 +71,12 @@ app.controller("Posts", ["$scope", function($scope){
       image: "http://media.mnn.com/assets/images/2014/12/cliffs-of-moher.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       score: 0,
-      comments: []
+      comments: [],
+      date: new Date(),
+      addComment: false,
+      showComments: false,
+      newCommentAuthor: "",
+      newCommentComment: ""
     },
     {
       id: 2,
@@ -56,7 +86,12 @@ app.controller("Posts", ["$scope", function($scope){
       image: "http://whc.unesco.org/uploads/thumbs/site_0151_0001-750-0-20090914164402.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       score: 0,
-      comments: []
+      comments: [],
+      date: new Date(),
+      addComment: false,
+      showComments: false,
+      newCommentAuthor: "",
+      newCommentComment: ""
     },
     {
       id: 3,
@@ -66,16 +101,15 @@ app.controller("Posts", ["$scope", function($scope){
       image: "http://www.daviddarling.info/images/mountain.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       score: 0,
-      comments: []
+      comments: [],
+      date: new Date(),
+      addComment: false,
+      showComments: false,
+      newCommentAuthor: "",
+      newCommentComment: ""
     }
   ];
-  $scope.view.sorter = "score";
-  $scope.sortBy = function(sorter) {
-    if (sorter == "score") {
-      $scope.view.reverse = true;
-    }
-    return $scope.view.sorter = sorter;
-  };
+
   $scope.upVote = function(id) {
     ++$scope.posts[id].score
   };
